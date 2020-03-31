9843ae7ab2e5b7981a09e20ae6b67f911bedf02d
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2020-03-31 14:16:29 +0530

    .

--- index.php
+++ index.php
@@ -9,22 +9,28 @@ if (isset($_POST["submit"])){
     }
     else{
         $email = $_POST["email"];
-        if (!preg_match('/^[a-zA-Z\s]+$/',$email)){
-            $errors["email"] = "Username is not valid";
-        }
+        
+            if ($email != "artemis"){
+                $errors["email"] = "Only a goddes can find what you seek";
+            }
     }
     if (empty($_POST["password"])){
         $errors["password"] = "Password can not be empty";
     }else{
         $password = $_POST["password"];
-        if (!preg_match('/^[a-zA-Z\s]+$/',$password)){
-            $errors["password"] = "Password is not valid";
-        }
+        
+            if ($password != "alohomora"){
+                $errors["password"] = "Muggles are not allowed! Obliviate";
+            }
+        
     }
     if (!array_filter($errors)){
-        header("Location: Homepage.html");
+        if($email = "artemis" and $password = "alohomora"){
+            header("Location: MainPage/index.html");
+        }
     }
 }
+
 
 ?>
 
@@ -36,65 +42,65 @@ if (isset($_POST["submit"])){
         <meta name="keywords" content="HTML,CSS,JavaScript">
         <meta name="author" content="PCCS">
         <meta name = "viewport" content = "width=device-width, initial scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel = "stylesheet" href = "Styles/LogInStyle.css">
         <script src="https://kit.fontawesome.com/35acfd843f.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Baloo+2:500&display=swap" rel="stylesheet">
         <title>Log in page</title>
     </head>
 
-    <body onload="listFunction()">
-        <img class = "background" src = "Sources/watercolor.jpg">
+    <body>
+        <img class = "background" src = "Sources/hogwarts.jpg">
         <div class = "container">
-            <div class = "img">
-                <img src = "Sources/personalization.svg">
-            </div>
             <div class = "login-container">
                 <div class = "form">
-                    <form action = "LogIn.php" method = "POST">
+                    <form action = "index.php" method = "POST">
 
                         <img class = "avatar" src = "Sources/profile_avatar.svg">
-                        <h2>Welcome</h2>
+                        <h2>Apparition </h2>
 
-                        <div class = "input-div one focus">
+                        <div class = "input-div one">
                             <div class = "i">
                                 <i class = "fas fa-user"></i>
                             </div>
-                            <div>
+                            <div class = "inputBox">
                                 <!--h5>Username</h5-->
                                 <input class = "input" name = "email" type = "text" placeholder="Username" 
                                 value = "<?php echo htmlspecialchars($email); ?>"> 
                             </div>
                             
                         </div>
-                        <div class = 'error1' id = "user_error">
+                        <div class = 'error' id = "user_error">
                                 <?php echo $errors["email"]; ?>
                         </div>
 
-                        <div class = "input-div two focus">
+                        <div class = "input-div two">
                             <div class = "i">
                                 <i class = "fas fa-lock"></i>
                             </div>
-                            <div>
+                            <div class = "inputBox">
                                 <!--h5>Password</h5-->
                                 <input class = "input" name = "password" type = "password" placeholder="Password" 
                                 id = "myPassword" value = "<?php echo htmlspecialchars($password); ?>">
                             </div>
                             <div class = "i-eye">
-                                <input type="checkbox" onclick="showPassword()">
-                                <i class="far fa-eye"></i>
+                                <button type = "button" class = "btn btn-sm btn-dark" data-toggle = "button"
+                                aria-pressed="false" autocomplete="off" onclick="showPassword()" id = "eye-btn">
+                                <i class="far fa-eye" id = "eye-icon"></i>
+                                </button>
                             </div>
-
                             
                         </div>
                         <div class = 'error' id = "password_error">
                             <?php echo $errors["password"]; ?>
                         </div>
+
                         <a href = "#">Forgot Password?</a>
-                        <input type ="submit" class = "btn" value = "Login" name = "submit"> 
+                        <input type ="submit" class = "btn-submit" value = "Alberto" name = "submit"> 
                     </form>
                 </div>
             </div>
         </div>
         <script type = "text/javascript" src = "js/LogIn.js"></script>        
     </body>
-</html>
+</html> 
